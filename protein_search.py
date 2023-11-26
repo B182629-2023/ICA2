31293025139524e93b91d144e653070b6ea360d6
Alice Aakerberg <s2012003@bioinfmsc5.ed.ac.uk>
2023-11-26 18:55:49 +0000

    Seq conservation code file completed and protein search code file edited and completed

--- protein_search.py
+++ protein_search.py
@@ -13,49 +13,48 @@ import pandas as pd
 while True:
 	while True:
 		tax_group = input("Enter taxonomy group\n")
-		tax_group_correct = input(f"\nYou entered '{tax_group}'. Is this correct? (yes/no)\n")
-		if tax_group_correct == "yes":
-			print(f"\n\nQuerying taxonomic group '{tax_group}'...\n")
+		confirm_tax_group = input("\nYou entered " + tax_group + ". Is this correct? (yes/no)\n")
+		if confirm_tax_group == "yes":
+			print(f"\n\nQuerying taxonomic group " + tax_group + "...\n")
 			break
-		elif tax_group_correct == "no":
+		elif confirm_tax_group == "no":
 			print(f"\nResetting taxonomic group...\n")
 		else:
 			print("\nInvalid input. Please enter yes or no.\n")
 
 	# Searches for entries with taxnomic group with the swissprot filter and collects accession IDs
-	query = f"'{tax_group}[Organism:exp] AND swissprot[filter]'"
-	subprocess.call("esearch -db protein -query " + query + " | efetch -format acc > accessions.txt", shell=True)
+	tax_query = f"'{tax_group}[Organism:exp] AND swissprot[filter]'"
+	subprocess.call("esearch -db protein -query " + tax_query + " | efetch -format acc > tax_accessions.txt", shell=True)
 
 	# Shows user the number of protein entries for taxonomic group and asks if they want to proceed
-	with open('accessions.txt', 'r') as accessions:
-		accs = accessions.read().rsplit('\n')
-		print("\nThere are " + str(len(accs)) + " protein entries for " + tax_group)
+	with open('tax_accessions.txt', 'r') as tax_accessions:
+		tax_accs = tax_accessions.read().rsplit('\n')
+		print("\nThere are " + str(len(tax_accs)) + " protein entries for " + tax_group)
 
-	accession_no_continue = input(f"\nWould you like to continue querying {tax_group}? (yes/no)\n")
-	if accession_no_continue == "yes":
+	continue_tax_group = input(f"\nWould you like to continue querying {tax_group}? (yes/no)\n")
+	if continue_tax_group == "yes":
 		# collects protein family and superfamily information from the GenPept summary and removes unwated whitespace for each protein entry then exits loop
                 # sometimes this esearch returns 500 internal server error. If this occurs, run the code again as it always works the second/third time.
 		print(f"\n\nFetching protein families... (this may take several minutes)\n")
-		subprocess.call("esearch -db protein -query " + query + " | efetch -format gp | grep -A 1 \"SIMILARITY\" | awk '{sub(/^[ \\t]+/,\"\");}1' > pf_raw.txt", shell=True)
+		subprocess.call("esearch -db protein -query " + tax_query + " | efetch -format gp | grep -A 1 \"SIMILARITY\" | awk '{sub(/^[ \\t]+/,\"\");}1' > pf_raw.txt", shell=True)
 		break
 
-	elif accession_no_continue == "no":
+	elif continue_tax_group == "no":
 	        # resets tax_group variable and returns to the start of the loop
 		print(f"\nResetting taxonomic group...\n")
 	else:
 		print("\nInvalid input. Please enter yes or no.\n")
 
 
-
+string = " family"
 
 # Cleans the esearch generated file, removing everything but the name of the protein family and outputting content into a new file.
 
 with open("pf_raw.txt", "r") as input_file, open("pf_clean.txt", "w") as output_file:
-	string = " family"
 	# Removes everything but the name of the protein family, superfamily and subfamily and splits all these onto different lines using '.' as a separator.
-	clean_file = input_file.read().replace("\n"," ").replace(". ","\n").replace("[SIMILARITY] Belongs to the ","\n").replace("--","").replace(" belongs to the ", "").split('\n')
+	clean_pfam = input_file.read().replace("\n"," ").replace(". ","\n").replace("[SIMILARITY] Belongs to the ","\n").replace("--","").replace(" belongs to the ", "").split('\n')
 	# stores rows in input file which contain the string 'family'
-	for row in clean_file:
+	for row in clean_pfam:
 		if string in row:
 			print(row)
 			output_file.write(row + "\n")
@@ -67,17 +66,16 @@ print("\nCounting protein families for " + tax_group + "...\n")
 
 
 # Creates a pandas dataframe with separator as \n.
-fam_df = pd.read_csv("pf_clean.txt")
-fam_df.columns = ['Family']
+df = pd.read_csv("pf_clean.txt")
+df.columns = ['Family']
 # Creates a new column removing the string 'family' from all protein family names
-fam_df['Protein Family'] = fam_df.apply(lambda x : x['Family'].split(' family')[0], axis=1)
-family_df = fam_df[['Protein Family']]
-print(str(len(fam_df)) + " out of " + str(len(accs)) + " protein entries contain protein family information.")
+df['Protein Family'] = df.apply(lambda x : x['Family'].split(' family')[0], axis=1)
+family_df = df[['Protein Family']]
+print(str(len(family_df)) + " out of " + str(len(tax_accs)) + " protein entries contain protein family information.")
 
 # Creates a new dataframe counting the number of unique protein families
-protein_family_df = family_df['Protein Family'].value_counts()
-print(str(len(protein_family_df)))
-row_number = len(protein_family_df)
+unique_family_df = family_df['Protein Family'].value_counts()
+row_number = len(unique_family_df)
 # Shows the user how many protein families are present in their taxonomic group
 print("\nThere are " + str(row_number) + " protein families in " + tax_group + ".\n")
 
@@ -90,37 +88,37 @@ print("\nThere are " + str(row_number) + " protein families in " + tax_group + "
 
 while True:
 	# Asks the user if they want to see the different protein families in their taxonomic group
-	pfam_table = input("Would you like to view the protein families present in " + tax_group + "? (yes/no)\n")
-	if pfam_table == "yes":
+	df_view = input("Would you like to view the protein families present in " + tax_group + "? (yes/no)\n")
+	if df_view == "yes":
 		while True:
 			while True:
 				# Asks how many top protein families they would like to view
-				table_size = input("\nEnter the number of protein families with the most proteins you would like to view. (e.g. 5, 10, 50 etc.)\n")
+				df_size = input("\nEnter the number of top protein families you would like to view. (e.g. 5, 10, 50 etc.)\n")
 				# If their requested number of protein families (X) is <= the total number of protein families
 				# print the top X protein families in the protein family count dataframe, exit loop
-				if int(table_size) <= int(row_number):
-					table_size_df = protein_family_df.head(int(table_size))
-					print(table_size_df)
+				if int(df_size) <= int(row_number):
+					df_view_size = unique_family_df.head(int(df_size))
+					print(df_view_size)
 					break
 				#If the requested number is > than the total number of protein families reset loop
-				elif int(table_size) > int(row_number):
+				elif int(df_size) > int(row_number):
 					print("There are " + str(row_number) + " protein families in " + tax_group + ". Please enter a smaller number.")
 				else:
 					print("Invalid input.")
 
 			# Asks if they want to view another number of top protein families
-			table2 = input("\nWould you like to view another number of protein families? (yes/no)\n")
+			df_view_2 = input("\nWould you like to view another number of protein families? (yes/no)\n")
 			# If yes, reset loop
-			if table2 == "yes":
+			if df_view_2 == "yes":
 				print("\n")
 			# If no, exit nested loop
-			elif table2 == "no":
+			elif df_view_2 == "no":
 				break
 			else:
 				print("Invalid input.")
 		break
 	# If the user does not want to see protein families, exit nested loop
-	elif pfam_table == "no":
+	elif df_view == "no":
 		break
 	else:
 		print("Invalid input.")
@@ -133,42 +131,37 @@ while True:
 # If they search for a superfamily e.g. kinase, all protein families with name kinase will be returned.
 
 while True:
-	while True:
-		pfam_interest = input("\nEnter protein family of interest\n")
-
-		# Finds the rows of the dataframe containing the protein family string and counts number of rows. This is case insensitive.
-		interest = family_df.loc[family_df['Protein Family'].str.contains(pfam_interest, case=False)].value_counts()
-
-		if len(interest) == 0:
-	                # If there are no results with sepcified name, they are asked to enter a different protein family namme.
-			print("\nThere are no proteins from the " + pfam_interest  + " family. Please enter a different search.\n")
-
-		else:
-			# Prints the number of protein families returned in their search
-			print("\nThere are " + str(len(interest)) + " protein families containing the name " + pfam_interest + " in " + tax_group)
-
+	pfam_df_search = input("\nWould you like to search for a specific protein family? (yes/no)\n")
+	if pfam_df_search == "yes":
 		while True:
-			# Asks if the user wants to view the protein family names and their counts.
-			interest_view = input("\nWould you like to view the protein family search results? (yes/no)\n")
-			if interest_view == "yes":
-				print(interest)
-				break
-			elif interest_view == "no":
+			while True:
+				pfam_interest = input("\nEnter protein family of interest\n")
+
+				# Finds the rows of the dataframe containing the protein family string and counts number of rows. This is case insensitive.
+				find_interest = family_df.loc[family_df['Protein Family'].str.contains(pfam_interest, case=False)].value_counts()
+
+				if len(find_interest) == 0:
+	                	# If there are no results with sepcified name, they are asked to enter a different protein family namme.
+					print("\nThere are no proteins from the " + pfam_interest  + " family. Please enter a different search.\n")
+				elif len(find_interest) > 0:
+					# Prints the number of protein families returned in their search
+					print("\nThere are " + str(len(find_interest)) + " protein families containing the name " + pfam_interest + " in " + tax_group)
+					print(find_interest)
+					break
+				else:
+					print("Something went wrong. Please try again or enter another protein family.")
+			pfam_df_search_2 = input("\nWould you like to search for another protein family? (yes/no)\n")
+			if pfam_df_search_2 == "yes":
+				pass
+			elif pfam_df_search_2 == "no":
 				break
 			else:
 				print("Invalid input.")
 		break
-	# Asks if they want to search for another protein family.
-	pfam_interest_2 = input("\nWould you like to search for another protein family? (yes/no)\n")
-	if pfam_interest_2 == "yes":
-		print("\n")
-	elif pfam_interest_2 == "no":
+	elif pfam_df_search == "no":
 		break
 	else:
-		print("Invalid input.")
-
-
-
+		print("Invalid input. Please enter yes/no.")
 
 # The user is now asked for the protein family they would like to query, sequences for their full query are generated.
 
@@ -179,68 +172,72 @@ proteinfam = []
 while True:
 	while True:
 		while True:
-			prot_family = input("\nPlease enter a protein family name to query.\n")
-			prot_family_correct = input("\nYou entered " + prot_family + ". Is this correct? (yes/no)\n")
-			if prot_family_correct == "yes":
-				print("\nQuerying taxonomic group " + tax_group + " and protein family " + prot_family + " ...\n")
+			protein_family = input("\nPlease enter a protein family name to query.\n")
+			confrim_protein_family = input("\nYou entered " + protein_family + ". Is this correct? (yes/no)\n")
+			if confrim_protein_family == "yes":
+				print("\nQuerying taxonomic group " + tax_group + " and protein family " + protein_family + " ...\n")
 				break
-			elif prot_family_correct == "no":
+			elif confrim_protein_family == "no":
 				print(f"\nResetting protein family...\n")
 			else:
 				print("Invalid input.")
 
 		# Creates a variable for the query to be used in the esearch.
-		pfam_query_df = family_df.loc[family_df['Protein Family'].str.contains(prot_family, case=False)]
-		new_query = f"'{tax_group}[Organism:exp] AND swissprot[filter] AND {prot_family}'"
-		subprocess.call("esearch -db protein -query " + new_query + " | efetch -format acc > prot_accessions.txt", shell=True)
-		print("Progress:")
-		with open('prot_accessions.txt', 'r') as input_file, open('new_prot_accessions.txt', 'w') as output_file:
-			prot_accessions = input_file.read().rsplit('\n')
-			for accession in prot_accessions:
-				p_family = subprocess.run("esearch -db protein -query " + accession + "[accession] | efetch -format gp | grep -A 1 \"SIMILARITY\" | awk '{sub(/^[ \\t]+/,\"\");}1'", shell=True, capture_output=True, text=True)
-				p_return = p_family.stdout.strip()
-				p_replaced = p_return.replace("\n", " ").replace(". ","\n").replace("[SIMILARITY] Belongs to the ","")
-				string_f = " family"
-				for row in p_replaced.split("\n"):
-					if string_f in row:
-						if prot_family.lower() in row.lower():
-							accessionid.append(accession)
-							proteinfam.append(row)
-							print(str(len(accessionid)) + "/" + str(len(pfam_query_df)) + "accessions queried.")
-							output_file.write(accession + "\t" + row + "\n")
-				if len(accessionid) < len(pfam_query_df):
-					pass
-				else:
-					print("\nQuery complete!\n")
-					print(str(len(accessionid)) + " " +  prot_family + " family entries identified for " + tax_group)
-					break
-
+		protein_family_df = family_df.loc[family_df['Protein Family'].str.contains(protein_family, case=False)]
+		query = f"'{tax_group}[Organism:exp] AND swissprot[filter] AND {protein_family}'"
+		subprocess.call("esearch -db protein -query " + query + " | efetch -format acc > prot_accessions.txt", shell=True)
+		with open('prot_accessions.txt', 'r') as input_file2, open('new_prot_accessions.txt', 'w') as output_file2:
+			prot_accessions = input_file2.read().rsplit('\n')
+			if len(prot_accessions) <= 1:
+				print("No proteins with name " + protein_family + " were found.")
+				break
+			elif len(prot_accessions) > 2:
+				for accession in prot_accessions:
+					acc_esearch = subprocess.run("esearch -db protein -query " + accession + "[accession] | efetch -format gp | grep -A 1 \"SIMILARITY\" | awk '{sub(/^[ \\t]+/,\"\");}1'", shell=True, capture_output=True, text=True)
+					esearch_result = acc_esearch.stdout.strip()
+					clean_result = esearch_result.replace("\n", " ").replace(". ","\n").replace("[SIMILARITY] Belongs to the ","")
+					for row in clean_result.split("\n"):
+						if string in row:
+							if protein_family.lower() in row.lower():
+								accessionid.append(accession)
+								proteinfam.append(row)
+								print(protein_family + ": " +str(len(accessionid)) + "/" + str(len(protein_family_df)) +  " identified.")
+								output_file2.write(accession + "\t" + row + "\n")
+					if len(accessionid) < len(protein_family_df):
+						pass
+					else:
+						print("\nAll proteins found!\n")
+						print(str(len(accessionid)) + " " +  protein_family + " family entries identified for " + tax_group)
+						break
+				break
+			else:
+				print("Something went wrong. Please try again.")
 		# Shows the user the number of protein entries for their desired taxonomic group and protein family
-		pfam_query_df2 = family_df.loc[family_df['Protein Family'].str.contains(prot_family, case=False)].value_counts()
-		if len(pfam_query_df2) > 1:
-			print("\n\nHere are the unique protein families which contain the name " + prot_family + " and their protein counts.\n")
-			print(pfam_query_df2)
+		protein_family_df_counts = family_df.loc[family_df['Protein Family'].str.contains(protein_family, case=False)].value_counts()
+		if len(protein_family_df_counts) > 1:
+			print("\n\nHere are the unique protein families which contain the name " + protein_family + " and their protein counts.\n")
+			print(protein_family_df_counts)
 			# If the user decides to choose 1 protein family, this will reduce the number of sequences used in the conservation analysis.
 			# This may also increase accuracy as only closely related proteins from one family are used.
-			print("\nYour query contains " + str(len(pfam_query_df2)) + " protein families.\nFor more accurate protein conservation results, it is recommended to query 1 protein family.\n")
+			print("\nYour query contains " + str(len(protein_family_df_counts)) + " protein families.\nFor more accurate protein conservation results, it is recommended to query 1 protein family.\n")
 			break
 		else:
 			break
 
-	accession2_no_continue = input("\nWould you like to continue with your query? (yes/no)\n")
-	if accession2_no_continue == "yes":
+	continue_protein_family = input("\nWould you like to continue with your query? (yes/no)\n")
+	if continue_protein_family == "yes":
 		# Collects the protein sequences in fasta format and stores in a fasta file
 		print("\nFetching protein sequences... (this may take several minutes)\n")
 		with open("sequences.fa", "w") as output_file:
 			for accession in accessionid:
-				seqs = subprocess.run("esearch -db protein -query " + accession + "[accession] | efetch -format fasta", shell=True, capture_output=True, text=True)
-				seqs_return = seqs.stdout.strip()
-				output_file.write(seqs_return + "\n")
+				seq_esearch = subprocess.run("esearch -db protein -query " + accession + "[accession] | efetch -format fasta", shell=True, capture_output=True, text=True)
+				seq_result = seq_esearch.stdout.strip()
+				output_file.write(seq_result + "\n")
 				print(accession)
-		print("\nProtein sequences generated!")
+		print("\nProtein sequences generated! - see 'sequences.fa'.")
 		break
 
-	elif accession2_no_continue == "no":
+	elif continue_protein_family == "no":
 		print(f"\nResetting protein family...\n")
 
 	else:
--- seq_conservation.py
+++ seq_conservation.py
@@ -1,7 +1,160 @@
 #!/usr/bin/python3
 
-import os
+import subprocess
 import pandas as pd
 
-os.system("clustalo -i sequences.fa -o aligned_seqs.fa")
+
+
+
+
+
+# Imports PROSITE database
+subprocess.call("wget ftp://ftp.expasy.org/databases/prosite/prosite.dat", shell=True)
+
+
+
+# Runs a multiple sequence alingment of chosen sequence set in clustal format
+subprocess.call("clustalo -i sequences.fa -o aligned_seqs.aln --outfmt=clustal", shell=True)
+print("\nClustering protein sequences...\n")
+
+
+
+# Asks the user if they would like to view the alignment output
+view_clustalo = input("Would you like to view the level of conservation between these sequences? (yes/no)\n") 
+if view_clustalo == "yes":
+	with open("aligned_seqs.aln", "r") as sequences:
+		content = sequences.read()
+		print(content)
+elif view_clustalo == "no":
+	pass
+else:
+	pass
+
+
+
+# Creates a function which splits all entries in the prosite database and creates a separate dictionaries for all entries
+def split_entry(entry):
+	rows = entry.split("\n")
+	prosite_entry = {}
+	for row in rows:
+		# Checks that each row has 2 values in order to create a key and value pair
+		linesplit = row.split(None, 1)
+		if len(linesplit) > 1:
+			# Creates key and value pairs
+			key, value = linesplit
+			# As DR key contains values running over multiple lines, this checks if a key for DR already exists when iterating over the rows
+			if key == "DR": 
+				if key in prosite_entry:
+					# If a DR key exists, the value (i.e. row) is appended to the DR key in the relevant prosite_entry dictionary
+					prosite_entry[key].append(value.strip())
+				else:
+					# If a DR key does not yet exist, it creates a key with the respecive value (i.e. row)
+					prosite_entry[key] = [value.strip()]
+			else:
+				# If the row does not contain DR key, continue as normal with other keys
+				prosite_entry[key] = value.strip()
+	return prosite_entry
+
+
+
+
+
+# This dictionary contains all prosite_entry sub-dictionaries with the PROSITE accession as the key of each prosite_entry dictionary.
+prosite_dict = {}
+
+with open("prosite.dat", "r") as prosite_file:
+	# The first 26 lines are introductory lines, so they are skipped
+	content = prosite_file.read()[26:]
+	# Each entry is split by // characters
+	entries = content.split("//\n")
+	for entry in entries:
+		rows = entry.split("\n")
+		entry_ac = None
+
+		# extract the AC (prosite accession) in entry
+		for row in rows:
+			if "AC   " in row:
+				entry_ac = row.split()[1]
+		# call function to create a nested dictionary with prosite accession as key and prosite_entry dictionaries as values.
+		if entry_ac:
+			prosite_dict[entry_ac] = split_entry(entry)
+
+
+
+
+
+# Extract chosen protein sequence set accessions to query against prosite dictionary
+with open("new_prot_accessions.txt", "r") as prot_accessions, open("protein_accessions.txt", "w") as new_acc_file:
+	content = prot_accessions.read().split("\n")
+	for row in content:
+		versions = row.split("\t")[0]
+		accessions = versions.split(".")[0]
+		new_acc_file.write("\n" + accessions)
+
+
+
+prositeid = []
+acc = []
+prositedesc = []
+
+
+# For each accession, scans the dictionary for a match. If found, extracts prosite ID and motif description and appends to list. 
+with open("protein_accessions.txt", "r") as prot_accs:
+	content_2 = prot_accs.read().strip()
+	accs = content_2.split("\n")
+	for accession_2 in accs:
+		for key, value in prosite_dict.items():
+			# Finds DR values in each prosite entry directory in nested dictionary AND finds accession string in DR values.
+			if 'DR' in value and any(accession_2 in DR_value for DR_value in value['DR']):
+				# Appends prosite accession ID
+				prositeid.append(value.get('AC'))
+				# Appends protein accession ID
+				acc.append(accession_2)
+				# Appends motif description
+				prositedesc.append(value.get('DE'))
+
+
+print("Query complete!")
+
+
+
+
+
+# Create a dataset and a pandas dataframe from prosite data matches.
+prosite_data = {'Protein Accession': acc, 'Prosite Accession': prositeid, 'Motif': prositedesc}
+prosite_df = pd.DataFrame(prosite_data)
+
+# Counts the unique prosite accessions found in the protein sequence set and sorts motifs in order of most to least number of sequences containing motif
+prosite_df_counts = prosite_df.groupby(['Prosite Accession', 'Motif'])['Protein Accession'].nunique().reset_index()
+unique_motifs = len(prosite_df_counts)
+prosite_df_counts_desc = prosite_df_counts.sort_values('Protein Accession', ascending=False)
+print(str(unique_motifs) + " prosite motifs identified for chosen sequence set.")
+
+
+
+
+# Asks the user how many prosite motifs found in sequence set they want to view
+while True:
+	while True:
+		view_prosite_df = input("How many top prosite motifs would you like to view? (e.g. 5, 10, 50 etc.)\n")
+		# If the requested number is <=  the total number of unique motifs, print dataset.
+		if int(view_prosite_df) <= int(unique_motifs):
+			prosite_df_count = prosite_df_counts_desc.head(int(view_prosite_df))
+			print(prosite_df_count)
+			break
+		# If requested number is smaller, ask to enter a smaller number.
+		elif int(view_prosite_df) > int(unique_motifs):
+			print(str(unique_motifs) + " prosite motifs identified for chosen sequence set. Please enter a smaller number.\n")
+		else:
+			print("Invalid input")
+	# Asks if  they wish to view another number
+ 	view_prosite_data2 = input("Would you like to view another number of prosite motifs? (yes/no)\n")
+	if view_prosite_data2 == "yes":
+		pass
+	elif view_prosite_data2 == "no":
+		break
+	else:
+		print("Invalid input.")
+
+
 
